e6dcf94ee3bf7443c549fca6dd7108f34dc029dc
jaugeni <jaugeni@gmail.com>
2017-01-18 16:41:46 -0500

    add fancy view

---
+++ SocialNetworkAppWithFirebase/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialNetworkAppWithFirebase
+//
+//  Created by YAUHENI IVANIUK on 1/18/17.
+//  Copyright © 2017 YauheniIvaniuk. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
