f0bdff78d79a5b0f8390d2dc85bfeb867384950a
jaugeni <jaugeni@gmail.com>
2017-01-19 17:36:41 -0500

    pods updates. UI fixed.

---
+++ SocialNetworkAppWithFirebase/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  SocialNetworkAppWithFirebase
+//
+//  Created by YAUHENI IVANIUK on 1/19/17.
+//  Copyright © 2017 YauheniIvaniuk. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var captionLbl: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+
+
+}
