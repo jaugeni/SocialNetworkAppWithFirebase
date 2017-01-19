5419cfccd313041fac2517b670938684b5f6e49b
jaugeni <jaugeni@gmail.com>
2017-01-19 18:40:08 -0500

    Completed creating Firebase DB users

--- SocialNetworkAppWithFirebase/SignInVC.swift
+++ SocialNetworkAppWithFirebase/SignInVC.swift
@@ -28,6 +28,27 @@ class SignInVC: UIViewController {
         }
     }
     
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("YI: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("YI: Successfully authenticated with Firebase")
+                if let user = user {
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
+                }
+            }
+        })
+    }
+    
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirbaseDBUsers(uid: id, userData: userData)
+        let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
+        print("YI: Data saved to keychain \(keychainResult)")
+        performSegue(withIdentifier: FEED_SEGUE, sender: nil)
+    }
+    
     @IBAction func facebookBtnPressed(_ sender: Any) {
         
         let facebookLogin = FBSDKLoginManager()
@@ -42,24 +63,10 @@ class SignInVC: UIViewController {
                 let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
                 self.firebaseAuth(credential)
             }
-            
         }
-        
     }
     
-    func firebaseAuth(_ credential: FIRAuthCredential) {
-        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
-            if error != nil {
-                print("YI: Unable to authenticate with Firebase - \(error)")
-            } else {
-                print("YI: Successfully authenticated with Firebase")
-                if let user = user {
-                    self.completeSignIn(id: user.uid)
-                }
-            }
-        })
-    }
-    
+ 
     @IBAction func singInPressed(_ sender: Any) {
         
         if let email = emailField.text, let pwd = pwdField.text {
@@ -67,7 +74,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("YI: Email user authenticatted with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -76,7 +84,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("YI: Successfully authenticated with Email Firebase.")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -85,10 +94,5 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
-        let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
-        print("YI: Data saved to keychain \(keychainResult)")
-        performSegue(withIdentifier: FEED_SEGUE, sender: nil)
-    }
 }
 
